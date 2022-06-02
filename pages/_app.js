12c8b5bc1ea5edef851aa5a0c1f243e3868d5610
cuitlazotlac <cuitlazotlac@gmail.com>
2022-06-02 00:31:06 -0400

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div style={{ padding: "100px" }}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
