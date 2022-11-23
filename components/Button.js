32ad54cc9513f31a98a47e2749910937aa81769c
cuitlazotlac <cuitlazotlac@gmail.com>
2022-11-22 23:14:58 -0500

    V2 app improvements

---
+++ components/Button.js
@@ -0,0 +1,29 @@
+import { css } from "@emotion/css";
+import { PINK } from "../theme";
+
+export function Button({ buttonText, onClick }) {
+  return (
+    <button className={buttonStyle} onClick={onClick}>
+      {buttonText}
+    </button>
+  );
+}
+
+const buttonStyle = css`
+  border: none;
+  outline: none;
+  margin-left: 15px;
+  color: #340036;
+  padding: 17px;
+  border-radius: 25px;
+  cursor: pointer;
+  font-size: 14px;
+  font-weight: 500;
+  background-color: rgb(${PINK});
+  transition: all 0.35s;
+  width: 240px;
+  letter-spacing: 0.75px;
+  &:hover {
+    background-color: rgba(${PINK}, 0.75);
+  }
+`;
