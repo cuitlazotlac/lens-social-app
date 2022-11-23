52a0ecd0b1d158d78c4424602aeda36ea2e89e77
cuitlazotlac <cuitlazotlac@gmail.com>
2022-11-23 01:21:01 -0500

    ready for a first run

--- next.config.js
+++ next.config.js
@@ -1,22 +1,37 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: ["ipfs.infura.io"],
+//   },
+// };
+
+// // module.exports = nextConfig
+
+// module.exports = {
+//   images: {
+//     domains: ["lens.infura-ipfs.io"],
+//     remotePatterns: [
+//       {
+//         protocol: "ipfs",
+//         hostname: "**",
+//         pathname: "**",
+//       },
+//     ],
+//   },
+// };
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["ipfs.infura.io"],
-  },
-};
-
-// module.exports = nextConfig
-
-module.exports = {
-  images: {
-    domains: ["lens.infura-ipfs.io"],
-    remotePatterns: [
-      {
-        protocol: "ipfs",
-        hostname: "**",
-        pathname: "**",
-      },
+    domains: [
+      "ipfs.infura.io",
+      "statics-polygon-lens-staging.s3.eu-west-1.amazonaws.com",
+      "lens.infura-ipfs.io",
+      "",
     ],
   },
 };
+
+module.exports = nextConfig;
